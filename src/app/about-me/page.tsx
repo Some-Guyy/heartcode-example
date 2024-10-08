c753bf1fc114314f72dbfbe6617faacfed5f0ea1
Some Guy <somenormaladdress@gmail.com>
2024-10-08 10:43:22 +0800

    initialise about-me and quiz pages

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+	return (
+		<div>hello world this is the about me</div>
+	);
+}
+
---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+	return (
+		<div>hello world this is the quiz</div>
+	);
+}
+
