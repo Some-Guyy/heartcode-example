68c806920aeb0a8932319182d4bc7ab796748658
Some Guy <somenormaladdress@gmail.com>
2024-10-08 11:34:50 +0800

    update about-me

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,6 +1,30 @@
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+import Rat from "@/app/assets/rat.jpg";
+
 export default function Home() {
 	return (
-		<div>hello world this is the about me</div>
+		<>
+			<div>hello world this is about me</div>
+			<Card>
+				<CardHeader>
+					<CardTitle>hi im owen</CardTitle>
+					<CardDescription>i like to eat</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<Image src={Rat} width={500} height={500} alt="rat image"></Image>
+					<div className="flex flex-row gap-2"><p className="font-bold">Name:</p>Owen</div>
+					<div className="flex flex-row gap-2"><p className="font-bold">Major:</p>Information Systems</div>
+					<div className="flex flex-row gap-2"><p className="font-bold">Hobbies:</p>I don't like to code</div>
+				</CardContent>
+			</Card>
+		</>
 	);
 }
 
