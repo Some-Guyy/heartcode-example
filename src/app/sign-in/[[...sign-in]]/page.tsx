fd763955dfaebb11d09766f44fe5181fa8e38ab8
Some Guy <somenormaladdress@gmail.com>
2024-10-09 15:10:52 +0800

    center signin/up and add themes to it

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,20 @@
+"use client"
+
 import { SignIn } from '@clerk/nextjs'
+import { useTheme } from "next-themes";
+import { dark, neobrutalism } from '@clerk/themes'
 
 export default function Page() {
+  const currentTheme = useTheme()
+
   return (
     <div className='flex flex-col justify-center h-dvh'>
       <div className='flex flex-col items-center'>
-        <SignIn />
+        <SignIn 
+        appearance={{
+          baseTheme: currentTheme.theme === "dark" ? dark : neobrutalism,
+        }}
+        />
       </div>
     </div>
   )
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,20 @@
+"use client"
+
 import { SignUp } from '@clerk/nextjs'
+import { useTheme } from "next-themes";
+import { dark, neobrutalism } from '@clerk/themes'
 
 export default function Page() {
+  const currentTheme = useTheme()
+
   return (
     <div className='flex flex-col justify-center h-dvh'>
       <div className='flex flex-col items-center'>
-        <SignUp />
+        <SignUp 
+        appearance={{
+          baseTheme: currentTheme.theme === "dark" ? dark : neobrutalism,
+        }}
+        />
       </div>
     </div>
   )
