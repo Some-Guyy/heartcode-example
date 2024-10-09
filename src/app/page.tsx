2db9ce3c0ac858effa844b897ad4f4d9c6f05f3d
Some Guy <somenormaladdress@gmail.com>
2024-10-09 10:50:48 +0800

    added more flipwords in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,37 @@ import StonedCat from "@/app/assets/stoned_cat.gif";
 import { FlipWords } from "@/components/ui/flip-words";
 
 export default function Home() {
-  const words: string[] = ["Don't", "Maybe", "You probably shouldn't", "Sometimes people", "My dad likes to", "I", "Never", "We like to", "We hate to"];
+  const headerWords: string[] = ["Don't", "Maybe", "You probably shouldn't", "Sometimes people", "My dad likes to", "I", "Never", "We like to", "We hate to"];
+  const subjectWords: string[] = ["I", "My friend Bobby", "My dad", "My uncle", "Your mum"];
+  const verbWords: string[] = ["took", "ingested", "inhaled", "digested", "consumed", "looked at"];
+  const amountWords: string[] = ["a tiny amount of", "a handful of", "a moderate amount of", "a bunch of", "a shitton of", "a truckload of"];
+  const drugWords: string[] = ["crack", "coke", "ice", "meth", "shrooms", "heroin", "koufu caifan", "water"];
+  const dayWords: string[] = ["last month", "yesterday", "just now", "last week", "10 minutes ago", "2 days ago"];
+  const resultWords: string[] = ["puked non-stop after", "had explosive diarrhea", "won the lottery", "died", "slept and never woke again", "are still addicted to this day", "fainted on the spot", "hated it"]
+
   return (
     <div className="flex flex-col justify-evenly h-dvh">
       <div className="flex flex-col items-center p-8">
-        <div className="font-bold text-5xl py-4"><FlipWords words={words}/>do drugs kids</div>
+        <div className="font-bold text-5xl py-4"><FlipWords words={headerWords}/>do drugs kids</div>
         <p className="text-sm">drug bad</p>
       </div>
       <div className="flex flex-col items-center p-8">
         <div><Image src={CatSniff} width={500} alt="catsniff" /></div>
         <p className="font-bold text-4xl py-4">What are drugs?</p>
-        <p>Drugs are substances that alter the normal functioning of the body or mind. They can be naturally occurring or synthetically produced and are used for various purposes, such as treating illnesses, relieving pain, or improving mental and physical performance. Drugs come in different forms, including pills, liquids, and powders, and can be classified into categories such as prescription drugs, over-the-counter medications, and illicit drugs. While some drugs are beneficial when used as prescribed by a doctor, others, especially when misused or abused, can have harmful effects on the body and mind.</p>
+        <p>Drugs are a kind of ingredient known as lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       </div>
       <div className="flex flex-col items-center p-8">
         <div><Image src={StonedCat} width={500} alt="stonedcat" /></div>
         <p className="font-bold text-4xl py-4">Why drugs are bad for helth</p>
-        <p>Drugs can be harmful to health when they are misused or abused. Illicit drugs or the misuse of prescription drugs can lead to physical dependence, addiction, and a wide range of negative health effects. Prolonged use of certain drugs can damage vital organs like the liver, heart, and brain, impair cognitive functions, and weaken the immune system. Additionally, drugs can cause psychological issues such as anxiety, depression, and paranoia. Overdose is a significant risk, which can lead to severe health complications or death. The impact on health is often long-term, affecting both the body and mental well-being.</p>
+        <div className="text-lg">
+          <FlipWords duration={6000} words={subjectWords}/>
+          <FlipWords duration={6000} words={verbWords}/>
+          <FlipWords duration={6000} words={amountWords}/>
+          <FlipWords duration={6000} words={drugWords}/>
+          <FlipWords duration={6000} words={dayWords}/>
+          ,&nbsp;&nbsp;and&nbsp;&nbsp;they
+          <FlipWords duration={6000} words={resultWords}/>
+        </div>
       </div>
     </div>
   );
