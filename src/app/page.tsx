bf4771d74ffad1f3639f3bbdac684f0c606c794e
Some Guy <somenormaladdress@gmail.com>
2024-10-09 09:39:19 +0800

    update quiz page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,18 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-evenly h-dvh">
       <div className="flex flex-col items-center p-8">
-        <p className="font-bold text-5xl py-4">dont do drugs kids</p>
+        <p className="font-bold text-5xl py-4">Don&apos;t do drugs kids</p>
         <p className="text-sm">drug bad</p>
       </div>
       <div className="flex flex-col items-center p-8">
         <div><Image src={CatSniff} width={500} height={500} alt="catsniff" /></div>
         <p className="font-bold text-4xl py-4">What are drugs?</p>
-        <p className="text-sm">Drugs are substances that alter the normal functioning of the body or mind. They can be naturally occurring or synthetically produced and are used for various purposes, such as treating illnesses, relieving pain, or improving mental and physical performance. Drugs come in different forms, including pills, liquids, and powders, and can be classified into categories such as prescription drugs, over-the-counter medications, and illicit drugs. While some drugs are beneficial when used as prescribed by a doctor, others, especially when misused or abused, can have harmful effects on the body and mind.</p>
+        <p>Drugs are substances that alter the normal functioning of the body or mind. They can be naturally occurring or synthetically produced and are used for various purposes, such as treating illnesses, relieving pain, or improving mental and physical performance. Drugs come in different forms, including pills, liquids, and powders, and can be classified into categories such as prescription drugs, over-the-counter medications, and illicit drugs. While some drugs are beneficial when used as prescribed by a doctor, others, especially when misused or abused, can have harmful effects on the body and mind.</p>
       </div>
       <div className="flex flex-col items-center p-8">
         <div><Image src={StonedCat} width={500} height={500} alt="stonedcat" /></div>
         <p className="font-bold text-4xl py-4">Why drugs are bad for helth</p>
-        <p className="text-sm">Drugs can be harmful to health when they are misused or abused. Illicit drugs or the misuse of prescription drugs can lead to physical dependence, addiction, and a wide range of negative health effects. Prolonged use of certain drugs can damage vital organs like the liver, heart, and brain, impair cognitive functions, and weaken the immune system. Additionally, drugs can cause psychological issues such as anxiety, depression, and paranoia. Overdose is a significant risk, which can lead to severe health complications or death. The impact on health is often long-term, affecting both the body and mental well-being.</p>
+        <p>Drugs can be harmful to health when they are misused or abused. Illicit drugs or the misuse of prescription drugs can lead to physical dependence, addiction, and a wide range of negative health effects. Prolonged use of certain drugs can damage vital organs like the liver, heart, and brain, impair cognitive functions, and weaken the immune system. Additionally, drugs can cause psychological issues such as anxiety, depression, and paranoia. Overdose is a significant risk, which can lead to severe health complications or death. The impact on health is often long-term, affecting both the body and mental well-being.</p>
       </div>
     </div>
   );
--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,5 +1,105 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Input } from "@/components/ui/input"
+import { useToast } from "@/hooks/use-toast"
+
+const formSchema = z.object({
+	username: z.string().min(2, {
+		message: "Number must be at least 2 characters.",
+	}).max(10, {
+		message: "Number must be below 11 characters"
+	}),
+	q1: z.string({
+		required_error: "Please select an option"
+	})
+})
+
 export default function Home() {
+	const { toast } = useToast();
+
+	// 1. Define your form.
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			username: "",
+			q1: "",
+		},
+	})
+
+	// 2. Define a submit handler.
+	function onSubmit(data: z.infer<typeof formSchema>) {
+		if (data.q1 === "yes") {
+			toast({
+				title: `Congratulations ${data.username}`,
+				description: "You are drug dealer",
+			})
+		} else {
+			toast({
+				title: `Thank you ${data.username}`,
+				description: "Unfortunately you not drug dealer",
+			})
+		}
+	}
+
 	return (
-		<div>hello world this is the quiz</div>
-	);
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+				<FormField
+					control={form.control}
+					name="username"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Question 1</FormLabel>
+							<FormDescription>
+								What is yo name?
+							</FormDescription>
+							<FormControl>
+								<Input placeholder="what yo nam" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="q1"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Question 2</FormLabel>
+							<FormDescription>
+								Are you drug dealer?
+							</FormDescription>
+							<Select onValueChange={field.onChange} defaultValue={field.value}>
+								<FormControl>
+									<SelectTrigger>
+										<SelectValue placeholder="Please select an answer" />
+									</SelectTrigger>
+								</FormControl>
+								<SelectContent>
+									<SelectItem value="yes">Yes</SelectItem>
+									<SelectItem value="no">No</SelectItem>
+								</SelectContent>
+							</Select>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type="submit">Submit</Button>
+			</form>
+		</Form>
+	)
 }
