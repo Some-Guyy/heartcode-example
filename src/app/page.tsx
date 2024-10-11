34e789ebf84d01c6c3390b6f300f3c808cc3823c
Some Guy <somenormaladdress@gmail.com>
2024-10-11 16:03:52 +0800

    add typewriter effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import CatSniff from "@/app/assets/cat_sniff_pill.png";
 import StonedCat from "@/app/assets/stoned_cat.gif";
 import { FlipWords } from "@/components/ui/flip-words";
+import { TypewriterEffect } from "@/components/ui/typewriter-effect";
 
 export default function Home() {
   const headerWords: string[] = ["Don't", "Maybe", "You probably shouldn't", "Sometimes people", "My dad likes to", "I", "Never", "We like to", "We hate to"];
@@ -12,6 +13,9 @@ export default function Home() {
   const dayWords: string[] = ["last   month", "yesterday", "just   now", "last   week", "10   minutes   ago", "2   days   ago"];
   const resultWords: string[] = ["puked   non-stop   after", "had   explosive   diarrhea", "won   the   lottery", "died", "slept   and   never   woke   again", "are   still   addicted   to   this   day", "fainted   on   the   spot", "hated   it"]
 
+  const headerWords2 = [{text: "What"}, {text: "are"}, {text: "drugs?"}]
+  const headerWords3 = [{text: "Why"}, {text: "drugs"}, {text: "are"}, {text: "bad"}, {text: "for"}, {text: "helth"}]
+
   return (
     <div className="flex flex-col justify-evenly h-dvh">
       <div className="flex flex-col items-center p-8">
@@ -20,12 +24,12 @@ export default function Home() {
       </div>
       <div className="flex flex-col items-center p-8">
         <div><Image src={CatSniff} width={500} alt="catsniff" unoptimized /></div>
-        <p className="font-bold text-4xl py-4">What are drugs?</p>
+        <TypewriterEffect words={headerWords2} className="font-bold text-4xl py-4" />
         <p>Drugs are a kind of ingredient known as lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       </div>
       <div className="flex flex-col items-center p-8">
         <div><Image src={StonedCat} width={500} alt="stonedcat" unoptimized /></div>
-        <p className="font-bold text-4xl py-4">Why drugs are bad for helth</p>
+        <TypewriterEffect words={headerWords3} className="font-bold text-4xl py-4" />
         <div className="text-xl mx-auto">
           <FlipWords duration={6000} words={subjectWords}/>
           <FlipWords duration={6000} words={verbWords}/>
